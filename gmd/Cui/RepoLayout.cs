97318c3bb9779fcce9a882b9df7b9a1774a40de9
Michael Reichenauer <michael-reichenauer@users.noreply.github.com>
2022-11-01 07:51:24 +0000

    Start with repo layout

--- gmd/Cui/RepoLayout.cs
+++ gmd/Cui/RepoLayout.cs
@@ -1,7 +1,34 @@
+using gmd.Utils.Git;
+
 namespace gmd.Cui;
 
 
 interface IRepoLayout
 {
-
+    void SetText(IReadOnlyList<Commit> commits, ColorText text);
 }
+
+class RepoLayout : IRepoLayout
+{
+    public RepoLayout()
+    {
+    }
+
+    public void SetText(IReadOnlyList<Commit> commits, ColorText text)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            foreach (var c in commits)
+            {
+                text.Append(" ┃ ┃", Colors.Magenta);
+                text.Append($" {c.Sid}", Colors.Blue);
+                text.Append($" {c.Subject.Max(50),-50}", Colors.White);
+                text.Append($" {c.Author.Max(10),-10}", Colors.Green);
+                text.Append($" {c.AuthorTime.ToString().Max(10),-10}", Colors.DarkGray);
+                text.EoL();
+            }
+        }
+    }
+}
+
+
--- gmd/Cui/RepoView.cs
+++ gmd/Cui/RepoView.cs
@@ -14,15 +14,16 @@ interface IRepoView
 class RepoView : IRepoView
 {
     private readonly IGitService gitService;
-
+    private readonly IRepoLayout repoLayout;
     private TextViewEx textView;
     private ColorText text;
 
     public View View => textView;
 
-    RepoView(IGitService gitService) : base()
+    RepoView(IGitService gitService, IRepoLayout repoLayout) : base()
     {
         this.gitService = gitService;
+        this.repoLayout = repoLayout;
 
         text = new ColorText();
         textView = new TextViewEx(text)
@@ -47,19 +48,8 @@ class RepoView : IRepoView
             return;
         }
 
-        for (int i = 0; i < 10; i++)
-        {
-            foreach (var c in commits.Value)
-            {
-                text.Append(" ┃ ┃", Colors.Magenta);
-                text.Append($" {c.Sid}", Colors.Blue);
-                text.Append($" {c.Subject.Max(50),-50}", Colors.White);
-                text.Append($" {c.Author.Max(10),-10}", Colors.Green);
-                text.Append($" {c.AuthorTime.ToString().Max(10),-10}", Colors.DarkGray);
-                text.EoL();
-            }
+        repoLayout.SetText(commits.Value, text);
 
-            textView.Text = text.ToString();
-        }
+        textView.Text = text.ToString();
     }
 }
