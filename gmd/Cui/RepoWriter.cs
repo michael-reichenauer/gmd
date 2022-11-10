21f718e8dfff99f05dcf509ea8e74f77d504045a
Michael Reichenauer <michael-reichenauer@users.noreply.github.com>
2022-11-10 17:03:03 +0000

    wip

--- gmd/Cui/RepoWriter.cs
+++ gmd/Cui/RepoWriter.cs
@@ -135,7 +135,7 @@ class RepoWriter : IRepoWriter
         tips.Draw(0, maxWidth - 1);
         if (tips.Length > maxWidth - 1)
         {
-            Text.New.Dark("┄").Draw();
+            Text.New.Dark("┅").Draw();
         }
     }
 
@@ -225,7 +225,7 @@ class RepoWriter : IRepoWriter
 
             if (branchName.Length > maxTipNameLength)
             {   // Branch name to long, shorten it
-                branchName = "┄" + branchName.Substring(branchName.Length - maxTipNameLength);
+                branchName = "┅" + branchName.Substring(branchName.Length - maxTipNameLength);
             }
 
             var color = branchColorService.GetColor(repo, b);
