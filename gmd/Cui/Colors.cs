81cef51d90af3b84adef2d1b0f102d3f1b1204c8
Michael Reichenauer <michael-reichenauer@users.noreply.github.com>
2022-11-12 18:18:51 +0000

    Adjust colors of Info and Error message boxes

--- gmd/Cui/Colors.cs
+++ gmd/Cui/Colors.cs
@@ -47,6 +47,14 @@ class Colors
         Disabled = Colors.Dark,
     };
 
+    internal static readonly ColorScheme ErrorDialogColorScheme = new ColorScheme()
+    {
+        Normal = Colors.BrightRed,
+        Focus = Colors.White,
+        HotNormal = Colors.White,
+        HotFocus = Colors.White,
+        Disabled = Colors.Dark,
+    };
 
 
     internal static ColorFgBg Make(Terminal.Gui.Color fg)
@@ -58,4 +66,35 @@ class Colors
     {
         return View.Driver.MakeAttribute(fg, bg);
     }
+
+
+    // 		Colors.TopLevel.Normal = MakeColor (Color.BrightGreen, Color.Black);
+    // 		Colors.TopLevel.Focus = MakeColor (Color.White, Color.Cyan);
+    // 		Colors.TopLevel.HotNormal = MakeColor (Color.Brown, Color.Black);
+    // 		Colors.TopLevel.HotFocus = MakeColor (Color.Blue, Color.Cyan);
+    // 		Colors.TopLevel.Disabled = MakeColor (Color.DarkGray, Color.Black);
+
+    // 		Colors.Base.Normal = MakeColor (Color.White, Color.Blue);
+    // 		Colors.Base.Focus = MakeColor (Color.Black, Color.Gray);
+    // 		Colors.Base.HotNormal = MakeColor (Color.BrightCyan, Color.Blue);
+    // 		Colors.Base.HotFocus = MakeColor (Color.BrightBlue, Color.Gray);
+    // 		Colors.Base.Disabled = MakeColor (Color.DarkGray, Color.Blue);
+
+    // 		Colors.Dialog.Normal = MakeColor (Color.Black, Color.Gray);
+    // 		Colors.Dialog.Focus = MakeColor (Color.White, Color.DarkGray);
+    // 		Colors.Dialog.HotNormal = MakeColor (Color.Blue, Color.Gray);
+    // 		Colors.Dialog.HotFocus = MakeColor (Color.BrightYellow, Color.DarkGray);
+    // 		Colors.Dialog.Disabled = MakeColor (Color.Gray, Color.DarkGray);
+
+    // 		Colors.Menu.Normal = MakeColor (Color.White, Color.DarkGray);
+    // 		Colors.Menu.Focus = MakeColor (Color.White, Color.Black);
+    // 		Colors.Menu.HotNormal = MakeColor (Color.BrightYellow, Color.DarkGray);
+    // 		Colors.Menu.HotFocus = MakeColor (Color.BrightYellow, Color.Black);
+    // 		Colors.Menu.Disabled = MakeColor (Color.Gray, Color.DarkGray);
+
+    // 		Colors.Error.Normal = MakeColor (Color.Red, Color.White);
+    // 		Colors.Error.Focus = MakeColor (Color.Black, Color.BrightRed);
+    // 		Colors.Error.HotNormal = MakeColor (Color.Black, Color.White);
+    // 		Colors.Error.HotFocus = MakeColor (Color.BrightRed, Color.Gray);
+    // 		Colors.Error.Disabled = MakeColor (Color.DarkGray, Color.White);
 }
--- gmd/Cui/MainView.cs
+++ gmd/Cui/MainView.cs
@@ -37,71 +37,14 @@ class MainView : IMainView
 
         toplevel.Add(repoView.View);
 
-        // Terminal.Gui.Colors.Error.Normal = Colors.Red;
-        // Terminal.Gui.Colors.Error.Focus = Colors.White;
-        // Terminal.Gui.Colors.Error.HotNormal = Colors.Green;
-        // Terminal.Gui.Colors.Error.HotFocus = Colors.White;
-        // Terminal.Gui.Colors.Error.Disabled = Colors.Dark;
-
-        // Terminal.Gui.Colors.Dialog.Normal = View.Driver.MakeAttribute(Color.Black, Color.Gray);
-        // Terminal.Gui.Colors.Dialog.Focus = View.Driver.MakeAttribute(Color.White, Color.DarkGray);
-        // Terminal.Gui.Colors.Dialog.HotNormal = View.Driver.MakeAttribute(Color.Blue, Color.Gray);
-        // Terminal.Gui.Colors.Dialog.HotFocus = View.Driver.MakeAttribute(Color.BrightYellow, Color.DarkGray);
-        // Terminal.Gui.Colors.Dialog.Disabled = View.Driver.MakeAttribute(Color.Gray, Color.DarkGray);
-
-
-        // Terminal.Gui.Colors.Dialog.Normal = Colors.Blue;
-        // Terminal.Gui.Colors.Dialog.Focus = Colors.Green;
-        // Terminal.Gui.Colors.Dialog.HotNormal = Colors.Green;
-        // Terminal.Gui.Colors.Dialog.HotFocus = Colors.Yellow;
-        // Terminal.Gui.Colors.Dialog.Disabled = Colors.Dark;
+        Terminal.Gui.Colors.Dialog = Colors.DialogColorScheme;
+        Terminal.Gui.Colors.Error = Colors.ErrorDialogColorScheme;
 
         toplevel.Loaded += () => OnLoaded().RunInBackground();
         return toplevel;
     }
 
-    // public void CreateColors (bool hasColors = true)
-    // 	{
-    // 		Colors.TopLevel = new ColorScheme ();
-    // 		Colors.Base = new ColorScheme ();
-    // 		Colors.Dialog = new ColorScheme ();
-    // 		Colors.Menu = new ColorScheme ();
-    // 		Colors.Error = new ColorScheme ();
 
-    // 		if (!hasColors) {
-    // 			return;
-    // 		}
-
-    // 		Colors.TopLevel.Normal = MakeColor (Color.BrightGreen, Color.Black);
-    // 		Colors.TopLevel.Focus = MakeColor (Color.White, Color.Cyan);
-    // 		Colors.TopLevel.HotNormal = MakeColor (Color.Brown, Color.Black);
-    // 		Colors.TopLevel.HotFocus = MakeColor (Color.Blue, Color.Cyan);
-    // 		Colors.TopLevel.Disabled = MakeColor (Color.DarkGray, Color.Black);
-
-    // 		Colors.Base.Normal = MakeColor (Color.White, Color.Blue);
-    // 		Colors.Base.Focus = MakeColor (Color.Black, Color.Gray);
-    // 		Colors.Base.HotNormal = MakeColor (Color.BrightCyan, Color.Blue);
-    // 		Colors.Base.HotFocus = MakeColor (Color.BrightBlue, Color.Gray);
-    // 		Colors.Base.Disabled = MakeColor (Color.DarkGray, Color.Blue);
-
-    // 		Colors.Dialog.Normal = MakeColor (Color.Black, Color.Gray);
-    // 		Colors.Dialog.Focus = MakeColor (Color.White, Color.DarkGray);
-    // 		Colors.Dialog.HotNormal = MakeColor (Color.Blue, Color.Gray);
-    // 		Colors.Dialog.HotFocus = MakeColor (Color.BrightYellow, Color.DarkGray);
-    // 		Colors.Dialog.Disabled = MakeColor (Color.Gray, Color.DarkGray);
-
-    // 		Colors.Menu.Normal = MakeColor (Color.White, Color.DarkGray);
-    // 		Colors.Menu.Focus = MakeColor (Color.White, Color.Black);
-    // 		Colors.Menu.HotNormal = MakeColor (Color.BrightYellow, Color.DarkGray);
-    // 		Colors.Menu.HotFocus = MakeColor (Color.BrightYellow, Color.Black);
-    // 		Colors.Menu.Disabled = MakeColor (Color.Gray, Color.DarkGray);
-
-    // 		Colors.Error.Normal = MakeColor (Color.Red, Color.White);
-    // 		Colors.Error.Focus = MakeColor (Color.Black, Color.BrightRed);
-    // 		Colors.Error.HotNormal = MakeColor (Color.Black, Color.White);
-    // 		Colors.Error.HotFocus = MakeColor (Color.BrightRed, Color.Gray);
-    // 		Colors.Error.Disabled = MakeColor (Color.DarkGray, Color.White);
-    // 	}
 
     async Task OnLoaded()
     {
@@ -109,7 +52,7 @@ class MainView : IMainView
         // {
         //     return true;
         // });
-        //UI.InfoMessage("Title", "Text");
+        // UI.ErrorMessage("Title", "Text");
         // return;
 
         string path = "";  // !!!!!!!!!!!! make selectable 
