36068d3012d1d01c66c68e4f1dbbd5a2857cfc92
Michael Reichenauer <michael.reichenauer@gmail.com>
2022-11-28 08:50:45 +0000

    Adjust build.bat

--- build.bat
+++ build.bat
@@ -4,13 +4,17 @@ echo "Cleaning"
 del "gmd.exe"
 del "gmd_linux"
 del "gmd_osx"
+del "gmd_windows"
 
 
 if exist "gmd.exe" (
     echo "Error: gmd.exe was not deleted!";
     exit -1;
 ) 
-
+if exist "gmd_windows" (
+    echo "Error: gmd_windows was not deleted!";
+    exit -1;
+) 
 if exist "gmd_linux" (
     echo "Error: gmd_linux was not deleted!";
     exit -1;
@@ -24,6 +28,7 @@ if exist "gmd_osx" (
 echo "Building ...."
 dotnet publish gmd/gmd.csproj -c Release -r win-x64 -p:PublishReadyToRun=true --self-contained true -p:PublishSingleFile=true
 copy gmd\bin\Release\net6.0\win-x64\publish\gmd.exe gmd.exe
+copy gmd\bin\Release\net6.0\win-x64\publish\gmd.exe gmd_windows
 
 dotnet publish gmd/gmd.csproj -c Release -r linux-x64 -p:PublishReadyToRun=true --self-contained true -p:PublishSingleFile=true
 copy gmd\bin\Release\net6.0\linux-x64\publish\gmd gmd_linux
