4e959f503d0cbabf5aadfc66152a0592093f081a
Michael Reichenauer <michael.reichenauer@gmail.com>
2022-12-30 07:39:27 +0000

    Add install.sh script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+echo 'Downloading gmd ...'
+curl -sS -L --create-dirs -o ~/gmd/gmd "https://github.com/michael-reichenauer/gmd/releases/latest/download/gmd_linux" && chmod +x ~/gmd/gmd
+if ! grep -q 'export PATH=$PATH:~/gmd' ~/.profile; then
+  echo 'export PATH=$PATH:~/gmd' >>~/.profile
+fi
+
+. ~/.profile
+echo "gmd version: $(gmd --version)"
