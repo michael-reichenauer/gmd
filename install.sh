16fdd203ef38d8af468a2cfee264ccbcec1a6cd8
Michael Reichenauer <michael.reichenauer@gmail.com>
2022-12-31 05:45:21 +0000

    Adjust

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
  
+# usage: curl -sL https://raw.githubusercontent.com/michael-reichenauer/gmd/main/install.sh | bash
+# usage: curl -sL https://github.com/michael-reichenauer/gmd/releases/latest/download/install.sh | bash
+
 echo 'Downloading gmd ...'
 curl -sS -L --create-dirs -o ~/gmd/gmd "https://github.com/michael-reichenauer/gmd/releases/latest/download/gmd_linux" && chmod +x ~/gmd/gmd
 if ! grep -q 'export PATH=$PATH:~/gmd' ~/.profile; then
