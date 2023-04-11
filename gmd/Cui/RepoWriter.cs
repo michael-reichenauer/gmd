355b40fcc126b5f1301cde78ba2cd9eba72dcc42
Michael Reichenauer <michael.reichenauer@gmail.com>
2023-04-11 03:58:56 +0000

    Adjust subject, tips and tags column layout

--- gmd/Cui/RepoWriter.cs
+++ gmd/Cui/RepoWriter.cs
@@ -12,7 +12,7 @@ interface IRepoWriter
 class RepoWriter : IRepoWriter
 {
     static readonly int maxTipNameLength = 16;
-    static readonly int maxTipsLength = 41;
+    // static readonly int maxTipsLength = 41;
     const int markersWidth = 3; //  1 current marker and 1 ahead/behind and one space
 
     readonly IBranchColorService branchColorService;
@@ -49,7 +49,7 @@ class RepoWriter : IRepoWriter
             WriteGraph(text, graphRow, cw.GraphWidth);
             WriteCurrentMarker(text, c, isUncommitted);
             WriteAheadBehindMarker(text, c);
-            WriteSubject(text, cw, c, crb, branchTips, i == currentIndex);
+            WriteSubjectColumn(text, cw, c, crb, branchTips, i == currentIndex);
             WriteSid(text, cw, c);
             WriteAuthor(text, cw, c);
             WriteTime(text, cw, c);
@@ -132,72 +132,91 @@ class RepoWriter : IRepoWriter
         text.Black(" ");
     }
 
-    void WriteSubject(Text text, Columns cw, Commit c, Branch currentRowBranch,
+    void WriteSubjectColumn(Text text, Columns cw, Commit c, Branch currentRowBranch,
         IReadOnlyDictionary<string, Text> branchTips, bool isCurrent)
     {
-        int width = cw.Subject;
-        Text tagsText = Text.New;
-        if (c.Tags.Any())
-        {
-            string tags = "";
-            c.Tags.ForEach(t => tags += $"[{t.Name}]");
-            tags = tags.Max(Math.Max(0, width - 10));
-            tagsText.Green(tags);
-            width = width - tags.Length;
+        Text subjectText = GetSubjectText(c, isCurrent, currentRowBranch);
+        Text tagsText = GetTagsText(c);
+        Text tipsText = GetTipsText(c, branchTips);
+
+        int columnWidth = cw.Subject;
+
+        int subjectWidth = columnWidth;
+        int tipsWidth = 0;
+        int tagsWidth = 0;
+
+        // Calculate subject, tips and tags widths
+        if (columnWidth > 5 + 3 + 3)
+        {   // There is room for subject, tags and tips, check if shorten is needed 
+            if (subjectText.Length + tagsText.Length + tipsText.Length <= columnWidth)
+            {   // No need to shorten any of them, all fit
+                tagsWidth = tagsText.Length;
+                tipsWidth = tipsText.Length;
+                subjectWidth = columnWidth - tagsWidth - tipsWidth;
+            }
+            else if (Math.Min(subjectText.Length, 25) + tagsText.Length + tipsText.Length <= columnWidth)
+            {   // Just shorten the subject a bit (will show enaugh)
+                tagsWidth = tagsText.Length;
+                tipsWidth = tipsText.Length;
+                subjectWidth = columnWidth - tagsWidth - tipsWidth;
+            }
+            else
+            {   // Need to shorten tags and tips a bit too, but give tags and tips at least each 3 chars and max 20
+                subjectWidth = Math.Min(columnWidth - 3 - 3,
+                    columnWidth - Math.Min(tagsWidth + tipsWidth, 20));
+                int rest = (columnWidth - subjectWidth) / 2;
+                tagsWidth = Math.Min(tagsText.Length, columnWidth - subjectWidth - rest);
+                tipsWidth = columnWidth - subjectWidth - tagsWidth;
+            }
         }
 
-        int columnWidth = width;
-        int maxTipWidth = Math.Min(maxTipsLength, columnWidth - 5);
+        // Add a 'Branch set by user mark to subject if needed
+        if (c.IsBranchSetByUser) { subjectWidth -= 2; }
+        WriteSubText(text, subjectText, subjectWidth);
+        if (c.IsBranchSetByUser) { text.White(" Ф"); }
 
-        if (maxTipWidth < columnWidth - c.Subject.Length)
-        {
-            maxTipWidth = (columnWidth - c.Subject.Length);
-        }
-
-        if (branchTips.TryGetValue(c.Id, out var tips))
-        {
-            columnWidth -= Math.Min(tips.Length, maxTipWidth);
-            columnWidth -= 1;
-        }
-        if (c.IsBranchSetByUser)
-        {
-            columnWidth -= 2;
-        }
-
-        string subject = Txt(c.Subject, columnWidth);
-
-        if (c.IsUncommitted && isCurrent) { text.YellowSelected(subject); }
-        else if (isCurrent) { text.WhiteSelected(subject); }
-        else if (c.IsConflicted) { text.BrightRed(subject); }
-        else if (c.IsUncommitted) { text.BrightYellow(subject); }
-        else if (c.IsAhead) { text.BrightGreen(subject); }
-        else if (c.IsBehind) { text.BrightBlue(subject); }
-        else if (c.Id == Repo.PartialLogCommitID) { text.Dark(subject); }
-        else if (c.BranchCommonName == currentRowBranch.CommonName)
-        {
-            text.White(subject);
-        }
-        else { text.Dark(subject); }
-        text.Add(tagsText);
-
-        if (tips != null) { WriteBranchTips(text, tips, maxTipWidth); }
-        if (c.IsBranchSetByUser) { text.Dark(" Ф"); }
+        WriteSubText(text, tipsText, tipsWidth);
+        WriteSubText(text, tagsText, tagsWidth);
     }
 
-    void WriteBranchTips(Text text, Text tips, int maxWidth)
+    void WriteSubText(Text text, Text subText, int maxWidth)
     {
-        if (tips.Length > maxWidth - 1)
+        if (maxWidth == 0)
         {
-            maxWidth -= 2;
+            return;
+        }
+        if (subText.Length <= maxWidth)
+        {
+            text.Add(subText.Subtext(0, maxWidth, true));
+            return;
         }
 
-        text.Black(" ");
-        text.Add(tips.Subtext(0, maxWidth - 1));
-        if (tips.Length > maxWidth - 1)
-        {
-            text.Dark("┅");
-        }
+        // Shorten and add a '┅' char
+        text.Add(subText.Subtext(0, maxWidth - 1, true)).Dark("┅");
     }
+
+    Text GetSubjectText(Commit c, bool isCurrent, Branch currentRowBranch)
+    {
+        Text text = Text.New;
+        if (c.IsUncommitted && isCurrent) { text.YellowSelected(c.Subject); }
+        else if (isCurrent) { text.WhiteSelected(c.Subject); }
+        else if (c.IsConflicted) { text.BrightRed(c.Subject); }
+        else if (c.IsUncommitted) { text.BrightYellow(c.Subject); }
+        else if (c.IsAhead) { text.BrightGreen(c.Subject); }
+        else if (c.IsBehind) { text.BrightBlue(c.Subject); }
+        else if (c.Id == Repo.PartialLogCommitID) { text.Dark(c.Subject); }
+        else if (c.BranchCommonName == currentRowBranch.CommonName) { text.White(c.Subject); }
+        else { text.Dark(c.Subject); }
+
+        return text;
+    }
+
+    Text GetTagsText(Commit c) => c.Tags.Any()
+        ? Text.New.Green($" [{string.Join("][", c.Tags.Select(t => t.Name))}]") : Text.New;
+
+    Text GetTipsText(Commit c, IReadOnlyDictionary<string, Text> branchTips) =>
+        branchTips.ContainsKey(c.Id) ? Text.New.Black(" ").Add(branchTips[c.Id]) : Text.New;
+
 
     void WriteSid(Text text, Columns cw, Commit c)
     {
