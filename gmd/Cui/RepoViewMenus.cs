e8fb579887335bdf1b21b0e1b67771a120d70018
Michael Reichenauer <michael.reichenauer@gmail.com>
2023-05-17 17:56:41 +0000

    Merge only from one branch per commit

--- gmd/Cui/RepoViewMenus.cs
+++ gmd/Cui/RepoViewMenus.cs
@@ -367,6 +367,7 @@ class RepoViewMenus : IRepoViewMenus
         // Get all branches except current
         var branches = repo.Branches
              .Where(b => b.CommonName != currentName && b.PullMergeBranchName == "")
+             .DistinctBy(b => b.TipId)
              .OrderBy(b => b.CommonName);
 
         // Include commit if not on current branch
