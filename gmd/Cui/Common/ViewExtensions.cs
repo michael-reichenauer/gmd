86b8af7d4a2ee44859246e499e394ec1cb484701
Michael Reichenauer <michael.reichenauer@gmail.com>
2023-05-18 09:30:46 +0000

    Fix

--- gmd/Cui/Common/ViewExtensions.cs
+++ gmd/Cui/Common/ViewExtensions.cs
@@ -3,6 +3,6 @@ namespace Terminal.Gui;
 
 public static class ViewExtensions
 {
-    public static string GetText(this View source) => source?.Text?.ToString()?.Trim() ?? "";
+    public static string GetText(this TextField source) => source?.Text?.ToString()?.Trim() ?? "";
 }
 
