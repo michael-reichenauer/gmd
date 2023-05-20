b6069017debe412ec718a463e20c288b7f40e038
Michael Reichenauer <michael.reichenauer@gmail.com>
2023-05-20 05:14:02 +0000

    Fix Set branch name

--- gmd/Cui/SetBranch.cs
+++ gmd/Cui/SetBranch.cs
@@ -1,5 +1,4 @@
 using gmd.Cui.Common;
-using Terminal.Gui;
 
 namespace gmd.Cui;
 
@@ -11,44 +10,31 @@ interface ISetBranchDlg
 
 class SetBranchDlg : ISetBranchDlg
 {
-    TextField nameField = null!;
-
     public R<string> Show()
     {
-        Label infoLabel = Components.Label(1, 0, "Branch Name:");
-        nameField = Components.TextField(1, 1, 25, "");
-        var indicator = Components.TextIndicator(nameField);
+        var dlg = new UIDialog("Set Branch Name", 29, 7);
 
-        bool isOk = false;
-        Button okButton = Buttons.OK(true, () =>
+        dlg.AddLabel(1, 0, "Branch Name:");
+        var name = dlg.AddTextField(1, 1, 25, "");
+
+        dlg.AddOK(true, () =>
         {
-            if (Name() == "")
+            if (name.Text == "")
             {
                 UI.ErrorMessage("Empty tag name");
                 return false;
             }
-            isOk = true;
             return true;
         });
+        dlg.AddCancel();
 
-        var dialog = Components.Dialog("Set Branch Name", 29, 7, okButton, Buttons.Cancel());
-        dialog.Closed += e => UI.HideCursor();
-        dialog.Add(infoLabel, indicator, nameField);
-
-        nameField.SetFocus();
-        UI.ShowCursor();
-        UI.RunDialog(dialog);
-
-        if (!isOk)
+        if (!dlg.Show(name))
         {
             return R.Error();
         }
 
-        return Name();
+        return name.Text;
     }
-
-    private string Name() => nameField!.Text.ToString()?.Trim() ?? "";
-
 }
 
 
