d52b907b4ab9c2f59b79227d405a9268a0f019c9
Michael Reichenauer <michael.reichenauer@gmail.com>
2023-07-03 08:46:45 +0200

    Clean augservice

--- gmd/Server/Private/Augmented/Private/Augmenter.cs
+++ gmd/Server/Private/Augmented/Private/Augmenter.cs
@@ -28,55 +28,42 @@ class Augmenter : IAugmenter
     }
 
     public Task<WorkRepo> GetAugRepoAsync(GitRepo gitRepo, int truncateLimit)
-    {
+    {   // Run in background thread to not block the main thread since 
+        // this can take a long time for large repos and could be CPU intensive 
         return Task.Run(() => GetAugRepo(gitRepo, truncateLimit));
     }
+
 
     WorkRepo GetAugRepo(GitRepo gitRepo, int truncateLimit)
     {
         var status = ToStatus(gitRepo);
         WorkRepo repo = new WorkRepo(gitRepo.TimeStamp, gitRepo.Path, status);
 
-        SetAugBranches(repo, gitRepo);
-        SetAugCommits(repo, gitRepo, truncateLimit);
+        AddAugStashes(repo, gitRepo); // Must be done before adding augcommits
+        AddAugBranches(repo, gitRepo);
+        AddAugCommits(repo, gitRepo, truncateLimit);
+        AddAugTags(repo, gitRepo);
+
         branchStructureService.SetCommitBranches(repo, gitRepo);
-        SetAugTags(repo, gitRepo);
-        SetAugStashes(repo, gitRepo);
         SetBranchByName(repo);
-        AdjustDisplayNames(repo);
+
+        AdjustBranchDisplayNames(repo);
 
         return repo;
     }
 
 
-    void SetAugBranches(WorkRepo repo, GitRepo gitRepo)
+    void AddAugBranches(WorkRepo repo, GitRepo gitRepo)
     {
-        repo.Branches.AddRange(gitRepo.Branches.Select(b => new WorkBranch(b)));
+        // Convert git branches to intial augmented branches
+        var branches = gitRepo.Branches.Select(b => new WorkBranch(b));
+        repo.Branches.AddRange(branches);
 
-        // Set local name of all remote branches, that have a corresponding local branch as well
-        // Unset RemoteName of local branch if no corresponding remote branch (deleted on remote server)
-        foreach (var b in repo.Branches)
-        {
-            if (b.RemoteName != "")
-            {
-                var remoteBranch = repo.Branches.Find(bb => bb.Name == b.RemoteName);
-                if (remoteBranch != null)
-                {   // Corresponding remote branch, set local branch name property
-                    remoteBranch.LocalName = b.Name;
-                    if (b.IsCurrent)
-                    {
-                        remoteBranch.IsLocalCurrent = true;
-                    }
-                }
-                else
-                {   // No remote corresponding remote branch, unset property
-                    b.RemoteName = "";
-                }
-            }
-        }
+        SetLocalRemoteBranchNames(repo);
     }
 
-    void SetAugCommits(WorkRepo repo, GitRepo gitRepo, int truncateLimit)
+
+    void AddAugCommits(WorkRepo repo, GitRepo gitRepo, int truncateLimit)
     {
         IReadOnlyList<GitCommit> gitCommits = gitRepo.Commits;
         // For repositories with a lot of commits, only the latest 'truncateLimit' number of commits
@@ -86,48 +73,23 @@ class Augmenter : IAugmenter
         bool isTruncatedNeeded = false;
         repo.Commits.Capacity = gitCommits.Count;
 
-        // Iterate git commits in reverse
+        // Iterate git commits in reverse, to ensure parents are added before children
         for (var i = gitCommits.Count - 1; i >= 0; i--)
         {
             GitCommit gc = gitCommits[i];
-            if (repo.StashById.ContainsKey(gc.Id))
-            {
-                // Skip stash commits, will not be shown in log view 
-                continue;
-            }
+            if (IsStashCommit(repo, gc)) continue;   // not shown in log view
+
             WorkCommit commit = new WorkCommit(gc);
 
             if (isTruncatedPossible)
-            {
-                // The repo was truncated, check if commits have missing parents, which will be set
-                // to a virtual/fake "truncated commit"
-                if (commit.ParentIds.Count > 0)
-                {
-                    // Not a merge commit but check if parent is missing and need a truncated commit parent
-                    if (!repo.CommitsById.TryGetValue(commit.ParentIds[0], out var parent))
-                    {
-                        isTruncatedNeeded = true;
-                        commit.ParentIds[0] = Repo.TruncatedLogCommitID;
-                    }
-                }
-
-                if (commit.ParentIds.Count > 1)
-                {
-                    // Merge commit, check if parents are missing and need a truncated commit parent
-                    if (!repo.CommitsById.TryGetValue(commit.ParentIds[1], out var parent))
-                    {
-                        isTruncatedNeeded = true;
-                        commit.ParentIds[1] = Repo.TruncatedLogCommitID;
-                    }
-                }
+            {   // Check if parents need to be replaced with truncated commit
+                isTruncatedNeeded = FixTuncatedParents(repo, isTruncatedNeeded, commit);
             }
 
             commit.GitIndex = i;
             repo.Commits.Add(commit);
             repo.CommitsById[commit.Id] = commit;
         }
-
-
 
         // Set current commit if there is a current branch with an existing tip
         GitBranch? currentBranch = gitRepo.Branches.FirstOrDefault(b => b.IsCurrent);
@@ -143,17 +105,36 @@ class Augmenter : IAugmenter
         repo.Commits.Reverse();
 
         if (isTruncatedNeeded)
-        {
-            // Add a virtual/fake truncated commit, which some commits will have as a parent
-            string msg = "< ... log truncated, more commits exists ... >";
-            WorkCommit pc = new WorkCommit(
-                id: Repo.TruncatedLogCommitID, subject: msg, message: msg,
-                author: "", authorTime: new DateTime(1, 1, 1), parentIds: new string[0]);
-            pc.IsTruncatedLogCommit = true;
-            pc.GitIndex = repo.Commits.Count;
-            repo.Commits.Add(pc);
-            repo.CommitsById[pc.Id] = pc;
+        {   // Add a virtual truncated commit, which some commits will have as a parent
+            AddTruncatedVirtualCommit(repo);
         }
+    }
+
+    bool IsStashCommit(WorkRepo repo, GitCommit gc) => repo.StashById.ContainsKey(gc.Id);
+
+
+    void AddAugTags(WorkRepo repo, GitRepo gitRepo)
+    {
+        gitRepo.Tags.ForEach(t =>
+        {
+            var tag = new Tag(t.Name, t.CommitId);
+            if (repo.CommitsById.TryGetValue(t.CommitId, out var c))
+            {
+                c.Tags.Add(tag);
+            }
+        });
+    }
+
+
+    void AddAugStashes(WorkRepo repo, GitRepo gitRepo)
+    {
+        gitRepo.Stashes.ForEach(s =>
+        {
+            var stash = new Stash(s.Id, s.Name, s.Branch, s.parentId, s.indexId, s.Message);
+            repo.Stashes.Add(stash);
+            repo.StashById[s.Id] = stash;
+            repo.StashById[s.indexId] = stash;
+        });
     }
 
     void SetBranchByName(WorkRepo repo)
@@ -161,7 +142,7 @@ class Augmenter : IAugmenter
         repo.Branches.ForEach(b => repo.BranchByName[b.Name] = b);
     }
 
-    void AdjustDisplayNames(WorkRepo repo)
+    void AdjustBranchDisplayNames(WorkRepo repo)
     {
         Dictionary<string, int> branchNameCount = new Dictionary<string, int>();
 
@@ -207,31 +188,67 @@ class Augmenter : IAugmenter
             s.DeletedFiles, s.ConflictsFiles);
     }
 
-
-
-
-    void SetAugTags(WorkRepo repo, GitRepo gitRepo)
+    static bool FixTuncatedParents(WorkRepo repo, bool isTruncatedNeeded, WorkCommit commit)
     {
-        gitRepo.Tags.ForEach(t =>
-        {
-            var tag = new Tag(t.Name, t.CommitId);
-            if (repo.CommitsById.TryGetValue(t.CommitId, out var c))
+        // The repo was truncated, check if commits have missing parents, which will be set
+        // to a virtual "truncated commit"
+        if (commit.ParentIds.Count > 0)
+        {   // Check if first parent is missing and need a truncated commit parent
+            if (!repo.CommitsById.TryGetValue(commit.ParentIds[0], out var parent))
             {
-                c.Tags.Add(tag);
+                isTruncatedNeeded = true;
+                commit.ParentIds[0] = Repo.TruncatedLogCommitID;
             }
-        });
+        }
+
+        if (commit.ParentIds.Count > 1)
+        {   // Merge commit, check if second parent is missing and need a truncated commit parent
+            if (!repo.CommitsById.TryGetValue(commit.ParentIds[1], out var parent))
+            {
+                isTruncatedNeeded = true;
+                commit.ParentIds[1] = Repo.TruncatedLogCommitID;
+            }
+        }
+
+        return isTruncatedNeeded;
     }
 
-
-    void SetAugStashes(WorkRepo repo, GitRepo gitRepo)
+    static void AddTruncatedVirtualCommit(WorkRepo repo)
     {
-        gitRepo.Stashes.ForEach(s =>
+        // Add a virtual truncated commit, which some commits will have as a parent
+        string msg = "< ... log truncated, more commits exists ... >";
+        WorkCommit pc = new WorkCommit(
+            id: Repo.TruncatedLogCommitID, subject: msg, message: msg,
+            author: "", authorTime: new DateTime(1, 1, 1), parentIds: new string[0]);
+        pc.IsTruncatedLogCommit = true;
+        pc.GitIndex = repo.Commits.Count;
+        repo.Commits.Add(pc);
+        repo.CommitsById[pc.Id] = pc;
+    }
+
+    static void SetLocalRemoteBranchNames(WorkRepo repo)
+    {
+        // Set local name of all remote branches, that have a corresponding local branch as well
+        // Unset RemoteName of local branch if no corresponding remote branch (deleted on remote server)
+        foreach (var b in repo.Branches)
         {
-            var stash = new Stash(s.Id, s.Name, s.Branch, s.parentId, s.indexId, s.Message);
-            repo.Stashes.Add(stash);
-            repo.StashById[s.Id] = stash;
-            repo.StashById[s.indexId] = stash;
-        });
+            if (b.RemoteName != "")
+            {
+                var remoteBranch = repo.Branches.Find(bb => bb.Name == b.RemoteName);
+                if (remoteBranch != null)
+                {   // Corresponding remote branch, set local branch name property
+                    remoteBranch.LocalName = b.Name;
+                    if (b.IsCurrent)
+                    {   // Local branch is current, set property on remote branch as well
+                        remoteBranch.IsLocalCurrent = true;
+                    }
+                }
+                else
+                {   // No corresponding remote branch for local branch (deleted), unset property
+                    b.RemoteName = "";
+                }
+            }
+        }
     }
 }
 
