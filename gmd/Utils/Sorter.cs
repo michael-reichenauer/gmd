dcd741434047c89bfbea4813943525d1b42cf65d
Michael Reichenauer <michael.reichenauer@gmail.com>
2023-08-09 05:58:41 +0200

    Clean

--- gmd/Utils/Sorter.cs
+++ gmd/Utils/Sorter.cs
@@ -32,29 +32,4 @@ static class Sorter
             }
         }
     }
-
-    // private static void CustomSortOld<T>(IList<T> list, Func<T, T, int> comparer)
-    // {
-    //     for (int i = 0; i < list.Count; i++)
-    //     {
-    //         bool swapped = false;
-    //         T item = list[i];
-
-    //         for (int j = i + 1; j < list.Count; j++)
-    //         {
-    //             if (comparer(item, list[j]) > 0)
-    //             {
-    //                 T tmp = list[j];
-    //                 list.RemoveAt(j);
-    //                 list.Insert(i, tmp);
-    //                 swapped = true;
-    //             }
-    //         }
-
-    //         if (swapped)
-    //         {
-    //             i = i - 1;
-    //         }
-    //     }
-    // }
 }
