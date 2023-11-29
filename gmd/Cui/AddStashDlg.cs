94f697bd563c9ec293764ec1a7ccd70efae890b8
Michael Reichenauer <michael.reichenauer@gmail.com>
2023-11-29 05:22:16 +0100

    Adjust stash dialog with curren subject

--- gmd/Cui/AddStashDlg.cs
+++ gmd/Cui/AddStashDlg.cs
@@ -5,17 +5,17 @@ namespace gmd.Cui;
 
 interface IAddStashDlg
 {
-    R<string> Show();
+    R<string> Show(string message);
 }
 
 
 class AddStashDlg : IAddStashDlg
 {
-    public R<string> Show()
+    public R<string> Show(string message)
     {
         var dlg = new UIDialog("Stash", 44, 7);
         dlg.AddLabel(1, 0, "Stash Message:");
-        var name = dlg.AddTextField(1, 1, 40);
+        var name = dlg.AddTextField(1, 1, 40, $"on '{message}'");
 
         if (!dlg.ShowOkCancel(name)) return R.Error();
 
